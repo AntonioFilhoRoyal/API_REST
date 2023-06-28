6bb2b8ddbd9de691567a333718586dd0b3e120d8
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-06-28 19:51:28 -0300

    Query simple method

--- src/main/java/ex/api/system/repositories/PostRepository.java
+++ src/main/java/ex/api/system/repositories/PostRepository.java
@@ -3,9 +3,14 @@ package ex.api.system.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import ex.api.system.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
