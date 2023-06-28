fd5700308832e8aececc600d75eee7055d42be17
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-06-27 22:29:44 -0300

    Exceptions

--- src/main/java/ex/api/system/service/UserService.java
+++ src/main/java/ex/api/system/service/UserService.java
@@ -1,12 +1,14 @@
 package ex.api.system.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ex.api.system.domain.User;
 import ex.api.system.repositories.UserRepository;
+import ex.api.system.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 	
 	public List<User> finAll(){
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
 	}
 }
