0f7852a52da40ccd729f90d06bfe5c0b5015f767
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-06-28 09:44:42 -0300

    Post findById

---
+++ src/main/java/ex/api/system/service/PostService.java
@@ -0,0 +1,23 @@
+package ex.api.system.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ex.api.system.domain.Post;
+import ex.api.system.repositories.PostRepository;
+import ex.api.system.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
