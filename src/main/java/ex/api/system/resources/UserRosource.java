9ea7391086efaac1067d219e2aa11df2fb19b7df
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-06-27 22:50:17 -0300

    Impletation PUT

--- src/main/java/ex/api/system/resources/UserRosource.java
+++ src/main/java/ex/api/system/resources/UserRosource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,14 @@ public class UserRosource {
 	@DeleteMapping
  	public ResponseEntity<Void> delete(@PathVariable String id) {
 		userSevice.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = userSevice.fromDTO(objDto);
+		obj.setId(id);
+		obj = userSevice.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
